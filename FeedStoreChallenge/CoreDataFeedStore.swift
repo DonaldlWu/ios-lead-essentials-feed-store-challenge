9628f297af64975aa3bd0f3ac8fbbcb658b7350f
Dei Ren Wu <deirenwu1101@gmail.com>
2021-09-15 18:57:55 +0800

    Extract override old cache action to managed model to make code more readable.

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -45,8 +45,7 @@ public final class CoreDataFeedStore: FeedStore {
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
 		context.perform { [context] in
 			do {
-				try ManagedCache.find(in: context).map(context.delete)
-				let managedCache = ManagedCache(context: context)
+				let managedCache = try ManagedCache.newUniqueInstance(in: context)
 				managedCache.timestamp = timestamp
 				managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
 
@@ -76,6 +75,11 @@ internal class ManagedCache: NSManagedObject {
 		let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
 		request.returnsObjectsAsFaults = false
 		return try context.fetch(request).first
+	}
+
+	static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+		try find(in: context).map(context.delete)
+		return ManagedCache(context: context)
 	}
 }
 
